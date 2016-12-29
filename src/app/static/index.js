1201347593aeccf04089b3be437cefe659349945
Youngchan Kim <ovekyc@gmail.com>
2016-12-29 15:47:09 +0900

    Add script tag to HTML

--- src/app/static/index.js
+++ src/app/static/index.js
@@ -2,6 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Clip from '../view/clip';
 
-ReactDOM.render(
-  <Clip/>, document.getElementById('container')
-);
+ReactDOM.render(<Clip/>, document.getElementById('container'));
