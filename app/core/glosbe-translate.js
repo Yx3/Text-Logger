8ce0ffeafacb7a2303722b854d685c5f76cb46ef
Youngchan Kim <ovekyc@gmail.com>
2017-01-01 23:47:56 +0900

    Added glosibe service

--- app/core/glosbe-translate.js
+++ app/core/glosbe-translate.js
@@ -1,5 +1,22 @@
+import https from 'https';
+import setting from '../setting.json';
 
+const api = 'https://glosbe.com/gapi/translate?format=json&pretty=true';
 
 export default function (source) {
   const encoded = encodeURIComponent(source);
+  const addr = `${api}&from=${setting.glosbeSourceLanguage}&dest=${setting.glosbeTargetLanguage}&phrase=${encoded}`;
+  return new Promise((resolve, reject) => {
+    const buffer = [];
+    https.get(addr, res => {
+      if (res.statusCode !== 200) reject(new Error('Fail to translate'));
+
+      res.on('data', data => {
+        buffer.push(data);
+      });
+      res.on('end', () => resolve(buffer.toString()));
+    });
+  }).then(buffer => {
+    const json = JSON.parse(buffer);
+  }).catch(console.log);
 }
