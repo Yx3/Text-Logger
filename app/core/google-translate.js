34474922f3c539eccb359457b546ecc5e4cd109b
Youngchan Kim <ovekyc@gmail.com>
2017-01-01 14:28:31 +0900

    Added setting.json
    Refactoring google-translate module

---
+++ app/core/google-translate.js
@@ -0,0 +1,32 @@
+import https from 'https';
+import setting from '../setting.json';
+
+const addrTranslate = 'https://translate.googleapis.com/translate_a/single?client=gtx';
+
+function refineJsonString(src) {
+  const regex = new RegExp(',,*', 'g');
+  const replace = ',';
+  return src.replace(regex, replace);
+}
+
+export function translateClip(source) {
+  const encoded = encodeURIComponent(source);
+  const addr = `${addrTranslate}&sl=${setting.googleSourceLanguage}&tl=${setting.googleTargetLanguage}&dt=t&q=${encoded}`;
+
+  return new Promise((resolve, reject) => {
+    const buffer = [];
+    https.get(addr, res => {
+      if (res.statusCode !== 200) reject(new Error('Fail to translate'));
+
+      res.on('data', data => {
+        buffer.push(data);
+      });
+      res.on('end', () => resolve(buffer.toString()));
+    });
+  }).then(buffer => {
+    const str = refineJsonString(buffer);
+    const json = JSON.parse(str);
+    return json[0][0][0];
+  }).catch(console.log);
+}
+
--- app/core/main.js
+++ app/core/main.js
@@ -1,50 +1,24 @@
 import {app, globalShortcut, clipboard} from 'electron';
 import fs from 'fs';
 import menubar from 'menubar';
-import https from 'https';
+import setting from '../setting.json';
+import {translateClip} from './google-translate';
 
 const dir = process.cwd();
 const logPath = `${dir}/log.txt`;
 const indexPath = `file://${dir}/app/view/popup.html`;
-const addrTranslate = 'https://translate.googleapis.com/translate_a/single?client=gtx';
+const SERVICE = {
+  GOOGLE: 'google',
+  GLOSBE: 'glosbe'
+};
+
 const mb = menubar({index: indexPath});
-
-let enableTranslate = true;
-let sourceLanguage = 'auto';
-let targetLanguage = 'ko';
-
-function refineJsonString(src) {
-  const regex = new RegExp(',,*', 'g');
-  const replace = ',';
-  return src.replace(regex, replace);
-}
-
-function translateClip(source) {
-  const encoded = encodeURIComponent(source);
-  const addr = `${addrTranslate}&sl=${sourceLanguage}&tl=${targetLanguage}&dt=t&q=${encoded}`;
-
-  return new Promise((resolve, reject) => {
-    const buffer = [];
-    https.get(addr, res => {
-      if (res.statusCode !== 200) reject(new Error('Fail to translate'));
-
-      res.on('data', data => {
-        buffer.push(data);
-      });
-      res.on('end', () => resolve(buffer.toString()));
-    });
-  }).then(buffer => {
-    const str = refineJsonString(buffer);
-    const json = JSON.parse(str);
-    return json[0][0][0];
-  }).catch(console.log);
-}
 
 function saveContents() {
   const clip = clipboard.readText();
   console.log(clip);
 
-  if (enableTranslate) {
+  if (setting.enableServiceHook) {
     translateClip(clip)
       .then(translated => `${clip} => ${translated}\n`)
       .then(contents => {
@@ -66,7 +40,7 @@ mb.on('show', () => {
   mb.window.reload();
 });
 
-mb.app.on('window-all-closed', function(){
+mb.app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
   }
