ac8b469b8acfa1269e8e6713c62aa3d8ce3f7a92
Kim Yu Yeong <yuda1124@naver.com>
2017-01-17 13:29:51 +0900

    Refactoring view (#16)
    * add module to package
    
    * application view
    
    * fix a typing error
    
    * edit code base on comments

--- app/view/app-component.js
+++ app/view/app-component.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import ClipList from './clip-list';
 import Header from './header';
+import SettingView from './settingView';
 import autobind from 'autobind-decorator';
+import setting from '../setting.json';
+import {ipcRenderer} from 'electron';
+import update from 'react-addons-update';
 
 @autobind
 export default class AppComponent extends React.Component {
   constructor() {
     super();
     this.state = {
-      enableDelete: false
+      enableDelete: false,
+      currentScene: 'clipList',
+      // TODO : change it using router later
+      appSetting: {
+        launchOnStartup: false,
+        showNotification: false,
+        enableTranslation: setting.enableServiceHook
+      }
     };
   }
 
@@ -16,11 +27,31 @@ export default class AppComponent extends React.Component {
     this.setState({enableDelete: !this.state.enableDelete});
   }
 
+  setCurrentScene(scene) {
+    this.setState({currentScene: scene});
+  }
+
+  toggleEnableTranslation() {
+    ipcRenderer.send('enable-translate', !this.state.appSetting.enableTranslation);
+    this.setState({
+      appSetting: update(this.state.appSetting,
+        {enableTranslation: {$set: !this.state.appSetting.enableTranslation}}
+      )
+    });
+  }
+
   render() {
     return (
-      <div>
-        <Header setDeleteEnabled={this.setDeleteEnabled}/>
-        <ClipList enableDelete={this.state.enableDelete}/>
+      <div style={{height: window.innerHeight, background: '#fbfbfb'}}>
+        <Header setDeleteEnabled={this.setDeleteEnabled}
+                setCurrentScene={this.setCurrentScene}
+                currentScene={this.state.currentScene}/>
+        {this.state.currentScene === 'clipList' ?
+          <ClipList enableDelete={this.state.enableDelete}
+                    enableTranslation={this.state.appSetting.enableTranslation}/> :
+          <SettingView appSetting={this.state.appSetting}
+                       toggleEnableTranslation={this.toggleEnableTranslation}/>
+        }
       </div>
     );
   }
--- app/view/header.js
+++ app/view/header.js
@@ -15,19 +15,37 @@ export default class header extends React.Component {
                  style={{height: 12, width: 12}}/>
           </div>
           <div style={{flexBasis: 13, flexShrink: 0}}/>
-          <div style={{flexGrow: 285, color: '#ffffff', fontFamily: 'SanFranciscoText-Regular'}}>
-            Text Logger | Beta version
+          <div style={{flexGrow: 285}}>
+            <span style={{
+              fontSize: 14,
+              color: '#ffffff',
+              fontFamily: 'SanFranciscoText-Regular'
+            }}>Text Logger </span>
+            <span style={{
+              fontSize: 12,
+              color: '#ffffff',
+              fontFamily: 'SanFranciscoText-Regular'
+            }}>| Beta version</span>
           </div>
           <div style={{flexBasis: 18}}/>
           <div style={{flexBasis: 18}}>
+            {this.props.currentScene === 'clipList' ?
             <img src={'../resources/trash-closed/trash-closed@3x.png'}
                  style={{height: 14, width: 14}}
-                 onClick={this.props.setDeleteEnabled}/>
+                 onClick={this.props.setDeleteEnabled}/> : <div/>
+            }
           </div>
           <div style={{flexBasis: 14}}/>
           <div style={{flexBasis: 17}}>
-            <img src={'../resources/setting/setting@3x.png'}
-                 style={{height: 14, width: 14}}/>
+            {this.props.currentScene === 'clipList' ?
+            // TODO : change it using router later
+              <img src={'../resources/setting/setting@3x.png'}
+                   style={{height: 14, width: 14}}
+                   onClick={()=>this.props.setCurrentScene('setting')}/> :
+              <img src={'../resources/close/close@3x.png'}
+                   style={{height: 14, width: 14}}
+                   onClick={()=>this.props.setCurrentScene('clipList')}/>
+            }
          </div>
          <div style={{flexBasis: 17}}/>
         </div>
